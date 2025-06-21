da59d47eff6f4148d457a2de50c2046313e1ed7c
MayankSingh-2002 <mksnavodaya@gmail.com>
2025-06-21 09:34:02 +0530

    Initial commit - Bank Management System

---
+++ include/BankAccount.h
@@ -0,0 +1,40 @@
+#ifndef BANK_ACCOUNT_H
+#define BANK_ACCOUNT_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+class BankAccount {
+    private:
+        std::string bankName = "MSBOI";
+        int bankCode;
+        std::string accountHolderName;
+        std::string accountNumber;
+        std::string accountPassword;
+        std::string adhaarNumber;
+        std::string email;
+        std::string phoneNUmber;
+        std::string accountType;
+        double balance;
+        std::vector<std::string> transactionHistory;
+
+
+    public:
+        void createAccount();
+        void loginAccount();
+        void mainMenu();
+        void depositMoney();
+        void withdrawMoney();
+        void checkBalance();
+        void showAccountDetails();
+        void showTransactionHistory();
+        void saveToFile();
+        void loadFromFile();
+        void caluclateInterest();
+
+};
+
+
+#endif
